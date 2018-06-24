d4308b1989b5c569573e6b3227377c1f37ebfce0
ranranking <zhongqi.miao@berkeley.edu>
2018-06-24 14:21:44 -0700

    update

--- ResNet/ResNet_SUN397.py
+++ ResNet/ResNet_SUN397.py
@@ -14,28 +14,41 @@ from PIL import Image
 from util import *
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='plain', type=str)
+parser.add_argument('--epoch', default=10, type=int)
 parser.add_argument('--model_id', default='plain', type=str)
+parser.add_argument('--decay_epoch', default=30, type=int)
+parser.add_argument('--dropout', default=False)
+parser.add_argument('--dropout_rate', default=0.5, type=float)
+parser.add_argument('--fc_add_dim', default=4096, type=int)
+parser.add_argument('--caffe_weights', default=False)
 args = parser.parse_args()
 
 
 # ============================================
 # Misc
 LOG_DIR = './log'
+DATASET = args.dataset
 MODEL_ID = args.model_id
 DATALOADER_WORKERS = 4
 LEARNING_RATE = 0.01
 LR_DECAY_FACTOR = 0.1
-LR_DECAY_EPOCHS = 30
+LR_DECAY_EPOCHS = args.decay_epoch
+DROPOUT = args.dropout
+DROPOUT_RATE = args.dropout_rate
+FC_ADD_DIM = args.fc_add_dim
 MOMENTUM = 0.9
-EPOCHS = 60
-BATCH_SIZE = 256
+EPOCHS = args.epoch
+BATCH_SIZE = 256 
 DISPLAY_STEP = 10
 NUM_CLASSES = 397
+CAFFE_WEIGHTS = args.caffe_weights
 
 if not os.path.isdir(LOG_DIR):
     os.makedirs(LOG_DIR)
 
 print('model_id: %s' % MODEL_ID)
+
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -56,21 +69,16 @@ data_transforms = {
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'test': transforms.Compose([
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 }
 
 print('Load datasets.')
 
-datasets = {x: sun_dataset(txt_file='./%s_txt/sun397_%s_lt.txt' % (MODEL_ID, x), transform=data_transforms[x]) 
-            for x in ['train', 'val', 'test']} 
+datasets = {x: sun_dataset(txt_file='./%s_txt/sun397_%s_lt.txt' % (DATASET, x), transform=data_transforms[x]) 
+            for x in ['train', 'val']} 
 dataloaders = {x: torch.utils.data.DataLoader(datasets[x], batch_size=BATCH_SIZE, shuffle=True, num_workers=DATALOADER_WORKERS) 
-               for x in ['train', 'val', 'test']}
-dataset_sizes = {x: len(datasets[x]) for x in ['train', 'val', 'test']}
+               for x in ['train', 'val']}
+dataset_sizes = {x: len(datasets[x]) for x in ['train', 'val']}
 
 print('Done.')
 
@@ -79,29 +87,47 @@ print('Done.')
 # ============================================
 # Model Preparation
 
-# Load pretrained model
 print('Load pretrained ResNet.')
-resnet = torchvision.models.resnet152(pretrained=True)
+
+# Load pretrained model
+if CAFFE_WEIGHTS:
+    print('Model using caffe pretrained weights.')
+    resnet = torchvision.models.resnet152(pretrained=False)
+    weights = torch.load('./caffe_weights/resnet152.pth')
+    resnet.load_state_dict(weights)
+else:
+    print('Model using pytorch pretrained weights.')
+    resnet = torchvision.models.resnet152(pretrained=True)
+
 # Freeze all layers
 for param in resnet.parameters():
     param.requires_grad = False
 
 print('Done.')    
 
-# Reset the fc layer
-num_features = resnet.fc.in_features
-resnet.fc = nn.Linear(num_features, NUM_CLASSES)
 
-resnet = resnet.to(device)
+if DROPOUT:
+    print('Model using dropout.')
+    resnet = MyResNet(resnet, NUM_CLASSES, DROPOUT_RATE, FC_ADD_DIM)
+    # Optimizer only on the last fc layers
+    optimizer = optim.SGD(list(resnet.fc.parameters()) + list(resnet.fc_add.parameters()), lr=LEARNING_RATE, momentum=MOMENTUM)
+else:
+    print('Model not using dropout.')
+    # Reset the fc layer
+    num_features = resnet.fc.in_features
+    resnet.fc = nn.Linear(num_features, NUM_CLASSES)
+    # Optimizer only on the last fc layers
+    optimizer = optim.SGD(list(resnet.fc.parameters()), lr=LEARNING_RATE, momentum=MOMENTUM)
 
-# Loss function
+# Loss function 
 loss_function = nn.CrossEntropyLoss()
 
-# Optimizer only on the last fc layer
-optimizer = optim.SGD(resnet.fc.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
-
-# Decay LR by a factor of 0.1 every 30 epochs
+# Decay LR 
+print('Learning rate decay epoch: %s' % LR_DECAY_EPOCHS)
+print('Learning rate decay factor: %s' % LR_DECAY_FACTOR)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=LR_DECAY_EPOCHS, gamma=LR_DECAY_FACTOR)
+
+resnet = resnet.to(device)
 
 
 
@@ -112,4 +138,4 @@ resnet = train_model(model=resnet, dataloaders=dataloaders, batch_size=BATCH_SIZ
                      dataset_sizes=dataset_sizes, num_classes=NUM_CLASSES, loss_function=loss_function, 
                      optimizer=optimizer, scheduler=exp_lr_scheduler, 
                      device=device, num_epochs=EPOCHS, display_step=DISPLAY_STEP,
-                     model_id=MODEL_ID)
+                     log_dir=LOG_DIR, model_id=MODEL_ID)
--- ResNet/util.py
+++ ResNet/util.py
@@ -5,6 +5,9 @@ import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
 
 
 class sun_dataset (torch.utils.data.Dataset):
@@ -28,13 +31,51 @@ class sun_dataset (torch.utils.data.Dataset):
             
         return image, label
     
+    
+class MyResNet (nn.Module):
+    
+    def __init__ (self, pretrained_resnet, num_classes, drop_rate, fc_add_dim):
+        super().__init__()
+        self.pretrained_resnet = pretrained_resnet
+        
+        # Reset the fc layer
+        self.num_features = self.pretrained_resnet.fc.in_features
+        
+        self.fc_add = nn.Linear(self.num_features, fc_add_dim)
+        self.dropout = nn.Dropout(p=drop_rate)
+        self.fc = nn.Linear(fc_add_dim, num_classes)
+        
+        print('Intermediate fc dimension is: %d' % fc_add_dim)
+        print('Dropout rate is: %f' % drop_rate)
+        
+    def forward(self, x):
+        x = self.pretrained_resnet.conv1(x)
+        x = self.pretrained_resnet.bn1(x)
+        x = self.pretrained_resnet.relu(x)
+        x = self.pretrained_resnet.maxpool(x)
+
+        x = self.pretrained_resnet.layer1(x)
+        x = self.pretrained_resnet.layer2(x)
+        x = self.pretrained_resnet.layer3(x)
+        x = self.pretrained_resnet.layer4(x)
+
+        x = self.pretrained_resnet.avgpool(x)
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc_add(x))
+        x = self.dropout(x)
+        x = self.fc(x)
+
+        return x
+    
        
 def train_model (model, dataloaders, dataset_sizes, batch_size, num_classes, loss_function, 
-                 optimizer, scheduler, num_epochs, device, display_step, model_id=None):
+                 optimizer, scheduler, num_epochs, device, display_step, log_dir, model_id=None):
     
     # Deep copy model weights
     best_model_weights = copy.deepcopy(model.state_dict())
     best_acc = 0.0
+    best_epoch = 0
     
     # Start training
     training_step = 0
@@ -53,8 +94,8 @@ def train_model (model, dataloaders, dataset_sizes, batch_size, num_classes, los
             running_loss = 0.0
             running_correct_total = 0
             
-            class_correct = [0. for i in range(num_classes)]
-            class_total = [0. for i in range(num_classes)]
+            class_correct = torch.tensor([0. for i in range(num_classes)])
+            class_total = torch.tensor([0. for i in range(num_classes)])
             
             # Iterate over data
             for inputs, labels in dataloaders[phase]:
@@ -81,45 +122,48 @@ def train_model (model, dataloaders, dataset_sizes, batch_size, num_classes, los
                         if training_step % display_step == 0:
                             minibatch_loss = loss.item()
                             minibatch_acc = (preds == labels).sum().item() / batch_size
-                            print('Epoch: %d, Step: %5d, Minibatch_loss: %.3f, Minibatch_accuracy_micro: %.3f' 
-                                  % (epoch, training_step, minibatch_loss, minibatch_acc))
+                            print('Epoch: [%d/%d], Step: %5d, Minibatch_loss: %.3f, Minibatch_accuracy_micro: %.3f' 
+                                  % (epoch, num_epochs, training_step, minibatch_loss, minibatch_acc))
                         
                 # Record loss and correct predictions
                 correct_tensor = (preds == labels).squeeze()
                 running_loss += loss.item() * inputs.shape[0]
                 running_correct_total += correct_tensor.sum().item()
                 
-                for i in range(batch_size):
+                for i in range(len(labels)):
                     label = labels[i]
                     class_correct[label] += correct_tensor[i].item()
                     class_total[label] += 1
                 
-            # Epoch loss and accuracies
+            # Epoch loss and accuracieds
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc_mic = running_correct_total / dataset_sizes[phase]
-            epoch_acc_mac = (class_correct[i] / class_total[i]).mean()
+            epoch_acc_mac = (class_correct / class_total).mean().item()
             
-            print('Epoch: %d, Phase: %s, Epoch_loss: %.3f, Epoch_accuracy_micro: %.3f, Epoch_accuracy_micro: %.3f' 
-                  % (epoch, phase, epoch_loss, epoch_acc_mic, epoch_acc_mac))
+            print('Epoch: [%d/%d], Phase: %s, Epoch_loss: %.3f, Epoch_accuracy_micro: %.3f, Epoch_accuracy_macro: %.3f' 
+                  % (epoch, num_epochs, phase, epoch_loss, epoch_acc_mic, epoch_acc_mac))
             
             # Deep copy the best model weights
             if phase == 'val' and epoch_acc_mic > best_acc:
+                best_epoch = epoch
                 best_acc = epoch_acc_mic
                 best_model_weights = copy.deepcopy(model.state_dict())
                 
     print()
     print('Training Complete.')
-    print('Best validation accuracy: %.3f' % best_acc)
+    print('Best validation accuracy is %.3f at epoch %d' % (best_acc, best_epoch))
     
     # Load the best model weights
     model.load_state_dict(best_model_weights)
     
     # Save the best model
     model_states = {'epoch': epoch + 1,
+                    'best_epoch': best_epoch,
+                    'state_dict_best': best_model_weights,
                     'state_dict': model.state_dict(),
                     'best_acc': best_acc,
                     'optimizer' : optimizer.state_dict()}
     
-    torch.save(model_states, 'model_%s_checkpoint.pth.tar' % model_id)
+    torch.save(model_states, os.path.join(log_dir, 'model_%s_checkpoint.pth.tar' % model_id))
     
     return model
